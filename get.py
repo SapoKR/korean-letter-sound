7e3a0d42e91aa23cf4cdd013451b55a2c79ad325
SapoKR <phmi1621@naver.com>
2023-05-28 21:35:30 +0900

    Update get.py

--- get.py
+++ get.py
@@ -1,16 +1,14 @@
 from gtts import gTTS
 
-n = 1
-for i in range(44032 + (n - 1), 44032 + 11172):
-    char = chr(i)
-    def save():
-        try:
-            tts = gTTS(char, lang='ko')
-            tts.save(f'./sound/{n}.mp3')
-        except:
-            print(f'Error! ({char} - {n}) Retrying..')
-            save()
+def save(char, n):
+    try:
+        tts = gTTS(char, lang='ko')
+        tts.save(f'./sound/{n}.mp3')
+    except:
+        print(f'Error! ({char} - {n}) Retrying..')
+        save(char, n)
 
-    save()
+for n in range(1, 11173):
+    char = chr(44031 + n)
+    save(char, n)
     print(f'{char} ( {n} / 11172 )')
-    n += 1
